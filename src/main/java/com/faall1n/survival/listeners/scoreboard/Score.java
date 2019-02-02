7681d67674f5730e1d3161916572ea9a0a2a02ac
Faall1n <leonardo.faall1n@gmail.com>
2019-02-02 17:49:19 -0200

    Survival

---
+++ src/main/java/com/faall1n/survival/listeners/scoreboard/Score.java
@@ -0,0 +1,70 @@
+package com.faall1n.survival.listeners.scoreboard;
+
+import com.faall1n.survival.Main;
+import com.faall1n.survival.utils.Helper;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public class Score implements Listener {	
+
+	public static void run() {
+
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				for (Player players : Bukkit.getOnlinePlayers()) {
+					update(players);
+				}
+			}
+		}.runTaskTimerAsynchronously(Main.getInstance(), 0L, 3L);
+
+	}
+
+	public static void build(Player p) {
+		Scoreboard sb = Bukkit.getScoreboardManager().getNewScoreboard();
+		Objective obj = sb.registerNewObjective("score", "dummy");
+
+		obj.setDisplayName("§6§lSURVIVAL");
+		obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+
+		LineAdder line = new LineAdder(sb, obj);
+
+		line.addLine("", "§2", "", 6);
+		line.addLine("", " §fGrupo: §4", "?", 5);
+		line.addLine("", "§1", "", 4);
+		line.addLine("", " §fJogadores: §a", "?", 3);
+		line.addLine("", " §fClã: §c", "?", 2);
+		line.addLine("", "§0", "", 1);
+		line.addLine("", "§6survival.com", "", 0);
+
+		p.setScoreboard(sb);
+	}
+
+	public static void update(Player p) {
+
+		Scoreboard sb = p.getScoreboard();
+		if (sb.getObjective("score") != null) {
+
+			Team grupo = sb.getTeam("line6");
+			grupo.setSuffix(Helper.getGroup(p) + "");
+
+			Team server = sb.getTeam("line2");
+			server.setSuffix(Helper.getOnline("0.0.0.0", 25565) + "");
+
+		}
+	}
+
+	@EventHandler
+	void evento(PlayerJoinEvent e) {
+		build(e.getPlayer());
+	}
+
+}
